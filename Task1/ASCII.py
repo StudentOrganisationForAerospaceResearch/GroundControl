89823567ff50a8c6876394ce4bc0d860e012f93c
MCobbe <murray.cobbe@ucalgary.ca>
2015-09-23 16:08:02 -0600

    Update ASCII.py

--- Task1/ASCII.py
+++ Task1/ASCII.py
@@ -18,3 +18,38 @@ print("Welcome to" + "\n" +
 ││││  └┬┘│ ││ │├┬┘  │  │ ││  │ ││ │├┬┘           
 ┴┘└┘   ┴ └─┘└─┘┴└─  └─┘└─┘┴─┘└─┘└─┘┴└─  
 """)
+#Ensures the Program does not continue until it has a good value for X.
+while True:
+    
+    #Prompts the user for an X coordinate
+    boardX = int(input("In which column would you like to play? "))
+
+    #Checks the user input for validity.
+    if(boardX>=8):
+        
+        #Prompts the user for a different input if their move in invalid.
+        print("Sorry, that is not a valid board position. Please use a number between 0 and 7.")
+    
+    else:
+        #if the users input is valid, continues the program. 
+        break
+
+#Ensures the Program does not continue until it has a good value for X.    
+while True:
+    
+    #Prompts the user for a Y coordinate
+    boardY = int(input("In which row would you like to play? "))
+
+    #Checks the user input for validity.
+    if(boardY>=8):
+        
+        #Prompts the user for a different input if their move in invalid.
+        print("Sorry, that is not a valid board position. Please use a number between 0 and 7.")
+    
+    else:
+        #if the users input is valid, continues the program. 
+        break
+
+#formats and prints the players requested move.
+print("Your piece will be played on square at X: " + str(boardX) + " Y: "  + str(boardY))
+
