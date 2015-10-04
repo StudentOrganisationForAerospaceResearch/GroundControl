9d63c341f3644c19060c99e0b7d5210bb1a64551
Sharjeel Junaid <sharjeel123@live.com>
2015-10-03 22:48:18 -0600

    Updated GUI.py - Renamed variables to be more relevant / accurate - Removed unnecessarily initialized / unused variables - Added more global variables to make it easier to customize the board & player piece colours - Initialized a list to store the player's latest piece coordinates (Python 2.6 requires mutable variables if you want to edit out of scope variables, so an integer does not work & this is the only real work around) - Added in 2 new functions to return the player's latest piece's Row & Column values as an integer - Changed the board setup dimensions (the board can now be scaled by changing a single variable) - Added scalability & the ability to customize the ASCII intro print out. It will automatically adjust to the boards dimensions & resize appropriately (can also set different fonts for it) - Added in a Main function to act as starter & input handler for the GUI

--- GUI.py
+++ GUI.py
@@ -17,11 +17,13 @@ import time
 from ASCII import printOutASCII
 
 # Initialize Variables
-boardTopLeftX = -250.0
-boardTopLeftY = -boardTopLeftX
-placePieceRow = 0.0
-placePieceColumn = 0.0
-moveCounter = 1
+halfBoardWidth = -250.0
+halfBoardHeight = -halfBoardWidth
+boardOutlineColour = "White"
+boardBackgroundColour = "Brown"
+player1Colour = "White"
+player2Colour = "Black"
+playerPieceCoordinates = [0, 0]
 
 # Initialize Board 8x8 Matrix
 boardMatrix = [[0 for boardMatrixIndex in range(9)] for boardMatrixIndex in range(9)]
@@ -29,11 +31,10 @@ boardMatrix = [[0 for boardMatrixIndex in range(9)] for boardMatrixIndex in rang
 # Initialize The Display Out & The First Turtle
 displayOut = turtle.Screen()
 turtle1 = turtle.Turtle()
-displayOut.bgcolor("Brown")
-displayOut.title("Reversi By Group 22")
-
-# Hides The Turtle Pointer
 turtle1.hideturtle()
+displayOut.bgcolor(boardBackgroundColour)
+displayOut.title("Reversi By Group 22")
+displayOut.setup(abs(halfBoardWidth * 2 + halfBoardWidth * 1 / 4), abs(halfBoardHeight * 2 + halfBoardHeight * 1 / 4))
 
 # Sets The Drawing Speed Of The Turtle & The Screen Print Out Delay To 0 (Instantaneous)
 turtle1.speed(0)
@@ -42,7 +43,8 @@ displayOut.delay(0)
 
 # Function To Print Out The ASCII Intro To The Display Overlay & Waits 10 Seconds Before Running The Program
 def printOutIntro():
-    turtle1.write(printOutASCII())
+    teleportToTile(5.5, 0.5)
+    turtle1.write(printOutASCII(), align="Left", font=("Arial", int(abs(halfBoardWidth) * 1 / 25)))
     time.sleep(10)
     turtle1.clear()
 
@@ -51,29 +53,29 @@ def printOutIntro():
 def printOutTable():
     # Clears Out The Display Overlay Before Printing Out The Table
     turtle1.clear()
-    # Sets The Turtle's Tracer Colour To White
-    turtle1.color("White")
+    # Sets The Turtle's Tracer Colour To The Defined Colour
+    turtle1.color(boardOutlineColour)
     # For Loop To Teleport The Turtle To Coordinates & Print The Reversi Table
     for indexCounter in range(9):
         # Lifts Up The Turtle & Prevents It From Leaving A Trail
         turtle1.up()
         # Teleports The Turtle To The Left Hand Side Of The Next Row
-        turtle1.goto(boardTopLeftX, boardTopLeftY - indexCounter * boardTopLeftY / 4)
+        turtle1.goto(halfBoardWidth, halfBoardHeight - indexCounter * halfBoardHeight / 4)
         # Lowers The Turtle & Allows It To Leave A Trail Again
         turtle1.down()
         # Prints Out The Row's Line
-        turtle1.forward(boardTopLeftX * -2)
+        turtle1.forward(-halfBoardWidth * 2)
 
         # Turns The Turtle Right To Print Out A Vertical Line
         turtle1.right(90)
         # Lifts Up The Turtle & Prevents It From Leaving A Trail
         turtle1.up()
         # Teleports The Turtle To The Top Of The Next Column
-        turtle1.goto(boardTopLeftX - indexCounter * boardTopLeftX / 4, boardTopLeftY)
+        turtle1.goto(halfBoardWidth - indexCounter * halfBoardWidth / 4, halfBoardHeight)
         # Lowers The Turtle & Allows It To Leave A Trail Again
         turtle1.down()
         # Prints Out The Row's Line
-        turtle1.forward(boardTopLeftY * 2)
+        turtle1.forward(halfBoardHeight * 2)
         # Turns The Turtle Back Left To Print Out The Horizontal Line
         turtle1.left(90)
     # Sets The Turtle's Tracer Colour To Black
@@ -98,9 +100,19 @@ def teleportToTile(inputRow, inputColumn):
     # Lifts Up The Turtle & Prevents It From Leaving A Trail
     turtle1.up()
     # Teleports To The Relevant Tile
-    turtle1.goto(boardTopLeftX - (inputColumn - 0.5) * boardTopLeftX / 4, boardTopLeftY - inputRow * boardTopLeftY / 4)
+    turtle1.goto(halfBoardWidth - (inputColumn - 0.5) * halfBoardWidth / 4, halfBoardHeight - inputRow * halfBoardHeight / 4)
     # Lowers The Turtle & Allows It To Leave A Trail Again
     turtle1.down()
+
+
+# Function To Return The Row The User Added His Piece To
+def getUserPieceRow():
+    return playerPieceCoordinates[0]
+
+
+# Function To Return The Column The User Added His Piece To
+def getUserPieceColumn():
+    return playerPieceCoordinates[1]
 
 
 # Function To Add A Piece To The Board
@@ -108,42 +120,51 @@ def addPieceToBoard(inputRow, inputColumn, moveCount):
     # Starts The Fill Command (To Fill In The Printed Circle)
     turtle1.begin_fill()
 
-    # If / Else Statement To Check Which Colour To Fill The Circle With (Odd Is Blue & Even Is Red) & It Also Stores Who Occupied The Tile
+    # If / Else Statement To Check Which Colour To Fill The Circle With & Also Stores Who Occupied The Tile
     if moveCount % 2 == 0:
-        # If The Number Is Even, Fills It Blue & Stores That They Occupied It
-        turtle1.fillcolor("White")
-        boardMatrix[inputRow][inputColumn] = "White"
+        # If The Number Is Even (Player 1), Fills It The Defined Colour & Stores That They Occupied It
+        turtle1.fillcolor(player1Colour)
+        boardMatrix[inputRow][inputColumn] = player1Colour
     else:
-        # If The Number Is Odd, Fills It Red & Stores That They Occupied It
-        turtle1.fillcolor("Black")
-        boardMatrix[inputRow][inputColumn] = "Black"
-        moveCount += 1
+        # If The Number Is Odd (Player 2), Fills It The Defined Colour & Stores That They Occupied It
+        turtle1.fillcolor(player2Colour)
+        boardMatrix[inputRow][inputColumn] = player2Colour
 
     # Prints Out A Circle In The Tile
-    turtle1.circle(-boardTopLeftX / 8)
+    turtle1.circle(abs(halfBoardWidth / 8))
     # Ends The Fill Command
     turtle1.end_fill()
 
-# Calls The Function To Print Out The Intro Message
-printOutIntro()
 
-# Calls Function To Print Out The Reversi Table
-printOutTable()
+# Main Function To Run The GUI
+def main():
+    # Initialize A Move Counter (Will Be Used To Determine Whose Turn It Is & Which Colour Piece To Place Down)
+    moveCounter = 1
 
-# Endless While Loop To Handle The User's Inputted Move
-while True:
-    # Prompts The User For Their Move's Location & Stores It In Variables (Pop Up Box)
-    placePieceRow = int(displayOut.numinput("Piece Row Input. Move Count: " + str(moveCounter), "Row You Would Like To Place A Piece In: ", minval = 1, maxval = 8))
-    placePieceColumn = int(displayOut.numinput("Piece Column Input. Move Count: " + str(moveCounter), "Column You Would Like To Place A Piece In: ", minval = 1, maxval = 8))
+    # Calls The Function To Print Out The Intro Message
+    printOutIntro()
 
-# Checks To See If The Coordinates Entered Is Valid & Then Performs The Move
-    if checkIfValidMove(placePieceRow, placePieceColumn):
-        moveCounter += 1
-        teleportToTile(placePieceRow, placePieceColumn)
-        addPieceToBoard(placePieceRow, placePieceColumn, moveCounter)
-    else:
-        # Tells The User That The Coordinates Entered Were Invalid
-        print("Invalid Move!")
+    # Calls Function To Print Out The Reversi Table
+    printOutTable()
 
-# Closes The Program If The GUI Overlay Is Clicked
-displayOut.exitonclick()
+    # Endless While Loop To Handle The User's Inputted Move
+    while True:
+        # Prompts The User For Their Move's Location & Stores It In Variables (Pop Up Box)
+        placePieceRow = int(displayOut.numinput("Piece Row Input. Move Count: " + str(moveCounter), "Row You Would Like To Place A Piece In: ", minval=1, maxval=8))
+        placePieceColumn = int(displayOut.numinput("Piece Column Input. Move Count: " + str(moveCounter), "Column You Would Like To Place A Piece In: ", minval=1, maxval=8))
+
+        # Checks To See If The Coordinates Entered Is Valid & Then Performs The Move & Stores The Coordinates
+        if checkIfValidMove(placePieceRow, placePieceColumn):
+            moveCounter += 1
+            teleportToTile(placePieceRow, placePieceColumn)
+            addPieceToBoard(placePieceRow, placePieceColumn, moveCounter)
+            # Stores The Row & Column Values Of The Piece That Is Being Added
+            playerPieceCoordinates[0] = placePieceRow
+            playerPieceCoordinates[1] = placePieceColumn
+
+    # Closes The Program If The GUI Overlay Is Clicked
+    displayOut.exitonclick()
+
+
+# Calls The Main Function To Run The GUI
+main()
