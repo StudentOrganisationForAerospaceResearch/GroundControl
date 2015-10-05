f81d488e0d99b07f19634446d4f01fd5631d4291
Murray Cobbe <murray.cobbe@ucalgary.ca>
2015-10-05 15:49:38 -0600

    Added findValids, this function still contains some logic errors.

--- ReversiBoard.py
+++ ReversiBoard.py
@@ -1,9 +1,11 @@
 
+#import GUI
 
 #initializes the 8x8 reversi board and assigns a value of 0 to every square.
 #0 = Empty square
 #1 = White piece
 #2 = black piece
+global board
 board = [[0 for i in range(8)] for i in range(8)]
 
 #Sets the four center squares to their start of game states.  
@@ -12,19 +14,30 @@ board[4][4] = 1
 board[3][4] = 2
 board[4][3] = 2
 
-turn = 1
+#printer = GUI()
 
 #To be used later to take player inputs.
 boardX = 0
 boardY = 0
 
 
+
+def main():
+    turn = 1
+    
+    while True:
+        turn += 1
+        getMove(0, 0, turn)
+
 #Finds and flips appropriate opponent in a direction of travel defined by dirX and dirY
     #Example: when dirX = 0 and dirY = -1 the squares directly below the new piece are checked. 
-def checkFlips(dirX, dirY):
+def checkFlips(dirX, dirY, X, Y, Player):
     
     scanX = 0
     scanY = 0
+    
+    boardX = X
+    boardY = Y
    
     while(-1<(boardX+scanX)<8 and -1<(boardY+scanY)<8):
                 
@@ -32,6 +45,8 @@ def checkFlips(dirX, dirY):
                 
                 scanX = scanX + 1
                 scanY = scanY + 1
+                print(boardX+scanX)
+                print(boardY+scanY)
                 if (board[boardX+(scanX*dirX)][boardY+(scanY*dirY)] == 0 ):
                     
                     break;
@@ -42,6 +57,9 @@ def checkFlips(dirX, dirY):
                     while scanX>=1 or scanY>=1:
                         
                         board[boardX+(scanX*dirX)][boardY+(scanY*dirY)] = Player
+                        
+                        #printer.addPieceToBoard((boardX+(scanX*dirX)), (boardY+(scanY*dirY)), Player)
+                        
                         scanX = scanX - 1
                         scanY = scanY - 1
    
@@ -83,9 +101,10 @@ def printBoard():
 
     
 
+    
 
-while True:
-#START getMove
+def getMove( X, Y, turn,):
+
     printBoard()
     
     Player = (turn%2)+1
@@ -128,14 +147,14 @@ while True:
         if (board[boardX][boardY] == 0):
             
             #Flips opponent pieces as need in all 8 axis from the played piece. 
-            checkFlips(1, 0)
-            checkFlips(0, 1)
-            checkFlips(-1, 0)
-            checkFlips(0, -1)
-            checkFlips(1, 1)
-            checkFlips(-1, -1)
-            checkFlips(-1, 1)
-            checkFlips(1, -1)
+            checkFlips(1, 0, boardX, boardY, Player)
+            checkFlips(0, 1, boardX, boardY, Player)
+            checkFlips(-1, 0, boardX, boardY, Player)
+            checkFlips(0, -1, boardX, boardY, Player)
+            checkFlips(1, 1, boardX, boardY, Player)
+            checkFlips(-1, -1, boardX, boardY, Player)
+            checkFlips(-1, 1, boardX, boardY, Player)
+            checkFlips(1, -1, boardX, boardY, Player)
                                     
             board[boardX][boardY] = Player
             print("Move Complete")
@@ -152,5 +171,5 @@ while True:
 #END GetMove
 
 
-
+main()
 print(board[boardX][boardY])
