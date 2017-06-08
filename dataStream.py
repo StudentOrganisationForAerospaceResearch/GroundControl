2a1132fa0a393c851c6cb7667a50e6fa690b427e
Nathan Meulenbroek <speedskatesoul@hotmail.ca>
2017-06-07 21:16:03 -0600

    Added data stream connection
    Credit to BoomKidneyshot

--- dataStream.py
+++ dataStream.py
@@ -11,16 +11,38 @@ Developers:
 Description:
 
 """
+# This is not a default module, make sure to install it!!!
+import serial
+import sys
 
 class DataStream():
     """
     Maintains data received from the downlink.
     
     """
+    ser = None
     
-    def __init__():
-        #TODO: Initialise data stream
-        return
+    def __init__(self):
+        if sys.platform == 'win32':
+            isWindows=True
+        else:
+            isWindows=False    
+        
+        if isWindows:      # This needs to be adjusted to the correct port name... []
+            #import msvcrt
+            self.ser=serial.Serial('COM4',timeout=1)
+        if not isWindows:
+            # Import the linux version
+            self.ser=serial.Serial('/dev/ttyUSB0')
+        return None
     
     
-    
+    def getData(self):
+        currentLine = self.ser.readline().rstrip()
+  
+        if len(currentLine) > 3 and currentLine[0] == '$' and currentLine[-1] == '*':
+            currentLine = currentLine[1:-1]
+            return currentLine
+
+        else:
+            return None
